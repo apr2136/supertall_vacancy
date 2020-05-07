12ea5dbdec9697f033867dc842656396e64eeb38
Glitch (supertall-vacancies) <none>
2020-05-07 08:07:30 +0000

    🏰👚 Checkpoint ./map.js:15803102/4492 ./styles.css:15803102/1200 ./index.html:15803102/1671

--- map.js
+++ map.js
@@ -562,6 +562,38 @@ map.on('mouseleave', 'turnstileData', function () {
 });
 
 
+var toggleableLayerIds = ['turnstileData'];
+
+
+for (var i = 0; i < toggleableLayerIds.length; i++) {
+    var id = toggleableLayerIds[i];
+
+    var link = document.createElement('a');
+    link.href = '#';
+    link.className = 'active';
+    link.textContent = id;
+
+    link.onclick = function(e) {
+        var clickedLayer = this.textContent;
+        e.preventDefault();
+        e.stopPropagation();
+
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+
+        if (visibility === 'visible') {
+            map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+            this.className = '';
+        } else {
+            this.className = 'active';
+            map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+        }
+    };
+
+    var layers = document.getElementById('menu');
+    layers.appendChild(link);
+}
+
+
 /* Here we watch for any resizing of the screen to
 adjust our scrolling setup */
 window.addEventListener("resize", scroller.resize);
