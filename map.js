6c03a83961a3a111dd1f18a68aabd93b5f76ea4a
Glitch (supertall-vacancies) <none>
2020-05-07 05:32:33 +0000

    🔦💘 Checkpoint ./map.js:15803102/3153

--- map.js
+++ map.js
@@ -189,55 +189,39 @@ map.on("load", function () {
             break;
         }
     }
-    // // Add the first new layer
-    // map.addLayer({
-    //     'id': 'turnstileData',
-    //     'type': 'circle',
-    //     'source': {
-    //         'type': 'geojson',
-    //         'data': 'data/turnstileData.geojson'
-    //     },
-    //     'paint': {
-    //         'circle-color': ['interpolate', ['linear'], ['get', 'ENTRIES_DIFF'],
-    //             -1, '#ff4400',
-    //             -0.7, '#ffba31',
-    //             -0.4, '#ffffff'
-    //         ],
-    //         'circle-stroke-color': '#4d4d4d',
-    //         'circle-stroke-width': 0.5,
-    //         'circle-radius': ['interpolate', ['exponential', 2], ['zoom'],
-    //             10, ['interpolate', ['linear'], ['get', 'ENTRIES_DIFF'],
-    //                 -1, 10,
-    //                 -0.4, 1
-    //             ],
-    //             15, ['interpolate', ['linear'], ['get', 'ENTRIES_DIFF'],
-    //                 -1, 25,
-    //                 -0.4, 12
-    //             ]
-    //         ],
-    //     }
-    // }, firstSymbolId);
-    // Add the second new layer
-    // map.addLayer({
-    //     'id': 'medianIncome',
-    //     'type': 'fill',
-    //     'source': {
-    //         'type': 'geojson',
-    //         'data': 'data/medianIncome.geojson'
-    //     },
-    //     'opacity': 0,
-    //     'paint': {
-    //         'fill-color': ['step', ['get', 'MHHI'],
-    //             '#ffffff',
-    //             20000, '#ccedf5',
-    //             50000, '#99daea',
-    //             75000, '#66c7e0',
-    //             100000, '#33b5d5',
-    //             150000, '#00a2ca'],
-    //         'fill-opacity': 0
-    //     }
-    // }, 'waterway-shadow');
 
+  //  chapter 7
+map.addLayer({
+       'id': 'landuseA4',
+       'type': 'fill',
+       'source': {
+           'type': 'geojson',
+           'data': 'data/landuseA4.geojson'
+       },
+       'paint': {
+           'fill-color': ['match', ['get', 'landUse'],
+               '04', '#800000',
+               '#800000',
+       ],'fill-opacity': 1
+       }
+   }, 'waterway-shadow');
+   map.addLayer({
+       'id': 'landuseB4',
+       'type': 'fill',
+       'source': {
+           'type': 'geojson',
+           'data': 'data/landuseB4.geojson'
+       },
+       'paint': {
+           'fill-color': ['match', ['get', 'landUse2'],
+               '04', '#fee8c8',
+               '#ffffff',
+       ],'fill-opacity': 0.5
+       }
+   }, 'waterway-shadow');
+  
+  
+  // chapter 8
     map.addLayer({
         'id': 'midtown',
         'type': 'fill',
@@ -251,9 +235,7 @@ map.on("load", function () {
         },
     }, 'waterway-shadow');
 
-    // var buildings = map.getLayer('building-extrusion')
-    // buildings['paint']['fill-extrusion-color']  = '#add8e6'
-    // buildings['paint']['fill-extrusion-opacity']  = 1
+
     map.setPaintProperty('building-extrusion', 'fill-extrusion-opacity', 0)
                          // ['step', ['get', 'height'],
     //             '#3182bd',
@@ -271,12 +253,12 @@ map.on("load", function () {
             'data': 'data/ch8/supertallheights.geojson'
         },
         'paint': {
-            'fill-extrusion-height': [ '*',['get', 'heightroof'], 0.3048],
+            'fill-extrusion-height': [ '*',['get', 'heightroof'], 0.3048],//convert to meters
             'fill-extrusion-color': 
           
           ['step', ['get', 'heightroof'],
                 '#3182bd',
-                1*0.3048, '#adadad',
+                0*0.3048, '#adadad',
                 250*0.3048, '#add8e6',
                 400*0.3048, '#3182bd',
                 600*0.3048, '#152238',
