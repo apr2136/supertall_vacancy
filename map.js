fc3d50f45b04320f69951858bcc4cfcec7a2d93d
Glitch (supertall-vacancies) <none>
2020-05-07 08:19:29 +0000

    🐢🐧 Checkpoint ./map.js:15803102/1125

--- map.js
+++ map.js
@@ -472,12 +472,22 @@ map.on("load", function() {
             'data': 'data/turnstileData.geojson'
         },
         'paint': {
-            'circle-color': '#ff7f50',
+            'circle-color': ['interpolate', ['linear'],
+                ['get', 'ENTRIES_DIFF'], -1, '#ff4400', -0.7, '#ffba31', -0.4, '#ffffff'
+            ],
             'circle-stroke-color': '#4d4d4d',
             'circle-stroke-width': 0.5,
-            'circle-radius': 5
+            'circle-radius': ['interpolate', ['exponential', 2],
+                ['zoom'],
+                10, ['interpolate', ['linear'],
+                    ['get', 'ENTRIES_DIFF'], -1, 10, -0.4, 1
+                ],
+                15, ['interpolate', ['linear'],
+                    ['get', 'ENTRIES_DIFF'], -1, 25, -0.4, 12
+                ]
+            ],
         }
-    });
+    }, firstSymbolId);
   
 // map.on('click', function(e) {
 //   console.log("hit the logging thing")
